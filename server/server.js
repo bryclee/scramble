cfaa98d8b0bb64b745cc1191ab3ad6ed2b4d33f3
Bryan Lee <pygmize.me@gmail.com>
2015-07-15 14:33:03 -0700

    [Initial] Sets up backend for word queries

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var wordnik = require('./wordnikApi/wordnikHelpers.js');
+
+app.use('/bower_components', express.static('bower_components'));
+app.use('/', express.static('public', {
+  index: 'index.html'
+}));
+
+app.get('/getRandom', function(req, res) {
+  wordnik.getRandomWords(function(data) {
+    res.send(data);
+  });
+});
+
+app.get('/searchWords/*', function(req, res) {
+  wordnik.searchWords(req.params[0], function(data) {
+    res.send(data);
+  });
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log('Server listening on', server.address().address, server.address().port);
+});
+
---
+++ server/wordnikApi/api_key.js
@@ -0,0 +1,1 @@
+module.exports = 'a0761a32c7b929378c969041faa02f1fffff4fef970b35fd7';
